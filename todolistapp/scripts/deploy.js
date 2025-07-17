61261a374db52f7149777f862873637c5ff84c7b
tayab52 <tayyab5122004@gmail.com>
2025-07-17 11:44:08 -0700

    smart contract complete

---
+++ todolistapp/scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+
+    const ToDoList = await hre.ethers.getContractFactory("ToDoList");
+    const toDoList = await ToDoList.deploy();
+
+    await toDoList.deployed();
+
+    console.log("Lock with 1 ETH deployed to:", toDoList.address);
+
+    console.log(toDoList);
+}
+
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1 ;
+});
