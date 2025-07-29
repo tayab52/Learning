82e0aee45458683311aff0e5dc0d72440b3670ba
tayab52 <tayyab5122004@gmail.com>
2025-07-29 11:04:33 -0700

    Starting FrontEnd in React

--- todolistapp/src/pages/index.js
+++ todolistapp/src/pages/index.js
@@ -1,18 +1,36 @@
  import React, {useEffect,useState,useContext } from "react";
+ import { MdVerified } from "react-icons/md";
+ import { IoIosSend,IoIosLock,IoIosUnlock } from "react-icons/io";
+import Image from 'next/image';
 
  // Internal Import
  import {ToDolistContext} from "../../context/ToDoListApp";
+ import Style from '../styles/index.module.css';
+ import Data from '../../components/Data';
 
  const Home = () => {
-    const {checkIfWalletIsConnect, toDolist} = useContext(ToDolistContext);
+    const {checkIfWalletIsConnect, connectWallet, toDolist, getToDoList,change,currentAccount,error,allToDolist,myList,allAddress} = useContext(ToDolistContext);
 
 
     useEffect(() => {
         checkIfWalletIsConnect();
-        toDolist();
+        
 
     },[]);
-  return <div>Home</div>
+  return (
+    <div className={Style.home}>
+      <div className={Style.navBar}>
+        <Image src={Loading} alt="Logo" width={50} height={50} />
+        <div className={Style.connect}>
+          {!currentAccount ? (
+            <button onClick={()=>connectWallet()}>Connect Wallet</button>
+          ):(
+            <button>{currentAccount.slice(0, 20)}...</button>
+          )}
+        </div>
+      </div>
+    </div>
+  )
  };
 
 
